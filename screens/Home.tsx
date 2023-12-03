7fff15287f28525e9db49747fd598d98fc9d5245
Piero Narciso <pieronarciso988@gmail.com>
2023-12-03 15:37:06 -0500

    fix: padding inset

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,14 @@
 import uuid from "react-native-uuid";
-import { Button, Text } from "tamagui";
+import { Button, Text, View } from "tamagui";
 import { useEffect, useState } from "react";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import { FlatList } from "react-native";
 import { AssistanceDeactivated } from "../types/assistance";
 import DateStatusItem from "../components/DateStatusItem";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const inset = useSafeAreaInsets();
   const [date, setDate] = useState(new Date());
   const [datesDeactivated, setDatesDeactivated] = useState<
     AssistanceDeactivated[]
@@ -39,13 +40,12 @@ const HomeScreen = () => {
   }, [datesDeactivated]);
 
   return (
-    <SafeAreaView>
+    <View style={{ paddingBottom: inset.bottom }}>
       <Text>{date.toDateString()}</Text>
       <Button onPress={() => showDatePicker()}>Agregar Fecha </Button>
 
       <FlatList
         data={datesDeactivated}
-        contentContainerStyle={{ paddingBottom: 90 }}
         renderItem={(item) => (
           <DateStatusItem
             key={item.item.id}
@@ -54,7 +54,7 @@ const HomeScreen = () => {
           />
         )}
       />
-    </SafeAreaView>
+    </View>
   );
 };
 
